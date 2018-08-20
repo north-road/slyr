1357c7f69c4c84bd983fdc1d87d58f90fd208268
Nyall Dawson <nyall.dawson@gmail.com>
2018-08-20 10:52:32 +1000

    Use override color lookup table for lab values were the standard conversion formula does not match ESRI's conversion results

--- parser/unit_test_color_parser.py
+++ parser/unit_test_color_parser.py
@@ -1,6 +1,7 @@
 import unittest
 from color_parser import (cielab_to_xyz, xyz_to_rgb, cielab_to_rgb)
 
+
 class TestColorParser(unittest.TestCase):
     """The LAB values are obtained from the color selector in ArcMap"""
 
@@ -53,3 +54,33 @@ class TestColorParser(unittest.TestCase):
         self.assertEqual(242.0, r)
         self.assertEqual(13.0, g)
         self.assertEqual(232.0, b)
+
+    def test_lab_to_rgb_lut(self):
+        r, g, b = cielab_to_rgb(
+            0.869,
+            14.067,
+            -21.3789)
+
+        self.assertEqual(0, r)
+        self.assertEqual(2, g)
+        self.assertEqual(20, b)
+
+    def test_lab_to_rgb_lut2(self):
+        r, g, b = cielab_to_rgb(
+            32.6742, 51.5019, 45.4267)
+
+        self.assertEqual(131, r)
+        self.assertEqual(2, g)
+        self.assertEqual(2, b)
+
+    def test_lab_to_rgb_lut3(self):
+        r, g, b = cielab_to_rgb(
+            32.67421111111, 51.50189999999, 45.4267000001)
+
+        self.assertEqual(131, r)
+        self.assertEqual(2, g)
+        self.assertEqual(2, b)
+
+
+if __name__ == '__main__':
+    unittest.main()
