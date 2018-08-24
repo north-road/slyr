06d1cf65d3fe6d8a2e23757b94f0982b93f2e213
Nyall Dawson <nyall.dawson@gmail.com>
2018-08-25 08:21:43 +1000

    Improve style_dump script

--- slyr/tools/style_dump.py
+++ slyr/tools/style_dump.py
@@ -10,18 +10,13 @@ from slyr.bintools.extractor import Extractor
 from slyr.parser.symbol_parser import read_symbol
 
 parser = argparse.ArgumentParser()
-parser.add_argument("file", help="style file to extract", nargs='?')
+parser.add_argument("file", help="style file to extract")
 args = parser.parse_args()
 
-if not args.file:
-    args.file = '/home/nyall/Styles/GEO_Surface___Solid_Shades.style'
+for symbol_type in (Extractor.FILL_SYMBOLS, Extractor.LINE_SYMBOLS, Extractor.MARKER_SYMBOLS):
+    print('{}:{}'.format(args.file, symbol_type))
 
-styles = [(args.file, Extractor.FILL_SYMBOLS)]
-
-for (fill_style_db, symbol_type) in styles:
-    print('{}:{}'.format(fill_style_db, symbol_type))
-
-    raw_symbols = Extractor.extract_styles(fill_style_db, symbol_type)
+    raw_symbols = Extractor.extract_styles(args.file, symbol_type)
     print('Found {} symbols of type "{}"\n\n'.format(len(raw_symbols), symbol_type))
 
     for index, symbol in enumerate(raw_symbols):
