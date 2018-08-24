3d04938122495bdb8802412673a37b2b32ae2b27
Nyall Dawson <nyall.dawson@gmail.com>
2018-08-25 08:21:31 +1000

    Add debug mode to bin_dump

--- slyr/tools/bin_dump.py
+++ slyr/tools/bin_dump.py
@@ -6,15 +6,25 @@ Converts a binary style file blob to a symbol and dumps its properties to the co
 
 import argparse
 import pprint
+import subprocess
 from slyr.parser.symbol_parser import read_symbol
 from slyr.converters.dictionary import DictionaryConverter
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="bin file to parse")
+parser.add_argument('--debug', help='Debug mode', action='store_true')
+
 args = parser.parse_args()
 
+if args.debug:
+    dump_args = ['hexdump',
+                 '-C',
+                   args.file]
+    result = subprocess.run(dump_args, stdout=subprocess.PIPE)
+    print(result.stdout.decode('UTF-8'))
+
 with open(args.file, 'rb') as f:
-    symbol = read_symbol(file_handle=f)
+    symbol = read_symbol(file_handle=f,debug=args.debug)
 
 converter = DictionaryConverter()
 pprint.pprint(converter.convert_symbol(symbol))
