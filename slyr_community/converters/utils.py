2710d7e280a31787b5555f2e09ee864e42b6d0fe
Nyall Dawson <nyall.dawson@gmail.com>
2020-11-30 09:53:07 +1000

    Resync some utility classes

--- slyr_community/converters/utils.py
+++ slyr_community/converters/utils.py
@@ -23,6 +23,9 @@ Conversion utilities
 """
 
 import math
+import os
+from pathlib import Path
+
 from qgis.PyQt.QtCore import Qt, QPointF
 
 
@@ -89,3 +92,56 @@ class ConversionUtils:
             'bevel': Qt.BevelJoin
         }
         return types[style]
+
+
+    @staticmethod
+    def path_insensitive(path):
+        """
+        Recursive part of path_insensitive to do the work.
+        """
+        return ConversionUtils._path_insensitive(path) or path
+
+    @staticmethod
+    def _path_insensitive(path):
+        """
+        Recursive part of path_insensitive to do the work.
+        """
+
+        if path == '' or os.path.exists(path):
+            return Path(path).resolve().as_posix() if path else path
+
+        path = Path(path).resolve().as_posix()
+
+        base = os.path.basename(path)  # may be a directory or a file
+        dirname = os.path.dirname(path)
+
+        suffix = ''
+        if not base:  # dir ends with a slash?
+            if len(dirname) < len(path):
+                suffix = path[:len(path) - len(dirname)]
+
+            base = os.path.basename(dirname)
+            dirname = os.path.dirname(dirname)
+
+        if not os.path.exists(dirname):
+            dirname = ConversionUtils._path_insensitive(dirname)
+            if not dirname:
+                return
+
+        # at this point, the directory exists but not the file
+
+        try:  # we are expecting dirname to be a directory, but it could be a file
+            files = os.listdir(dirname)
+        except OSError:
+            return
+
+        baselow = base.lower()
+        try:
+            basefinal = next(fl for fl in files if fl.lower() == baselow)
+        except StopIteration:
+            return
+
+        if basefinal:
+            return Path(os.path.join(dirname, basefinal) + suffix).as_posix()
+        else:
+            return
