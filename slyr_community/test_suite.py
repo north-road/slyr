970e62f7a28da3d380bf0dc930b6bfe3e087d3c7
Nyall Dawson <nyall.dawson@gmail.com>
2021-11-26 12:46:56 +1000

    Fix some lint warnings

--- slyr_community/test_suite.py
+++ slyr_community/test_suite.py
@@ -53,7 +53,7 @@ def _run_tests(test_suite, package_name, with_coverage=False):
     if with_coverage:
         cov.stop()
         cov.save()
-        report = tempfile.NamedTemporaryFile(delete=False)
+        report = tempfile.NamedTemporaryFile(delete=False)  # pylint: disable=consider-using-with
         cov.report(file=report)
         # Produce HTML reports in the `htmlcov` folder and open index.html
         # cov.html_report()
