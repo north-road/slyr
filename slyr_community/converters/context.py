59e6b3d7adb75b5cb077ba4226173eb0550e350a
Nyall Dawson <nyall.dawson@gmail.com>
2021-11-26 13:40:28 +1000

    Resync symbol conversion

--- slyr_community/converters/context.py
+++ slyr_community/converters/context.py
@@ -24,6 +24,7 @@ Conversion context
 
 import os
 import tempfile
+from typing import Optional
 
 from qgis.PyQt.QtCore import (
     QSettings,
@@ -34,6 +35,7 @@ from qgis.core import (
     QgsUnitTypes
 )
 
+from slyr.parser.exceptions import NotImplementedException
 
 class Context:
     """
@@ -49,6 +51,8 @@ class Context:
         self.file_name = ''
         self.dataset_name = ''
         self.symbol_name = ''
+        self.current_symbol = None
+        self.current_symbol_layer = None
         self.picture_folder = s.value('/plugins/slyr/picture_store_folder', '')
         self.temporary_picture_folder = ''
         self.style_folder = ''
@@ -61,11 +65,21 @@ class Context:
         self.convert_annotations = int(s.value('/plugins/slyr/enable_annotations', 0))
         self.convert_layouts = int(s.value('/plugins/slyr/convert_layouts', 1))
         self.convert_esri_fonts_to_simple_markers = int(s.value('/plugins/slyr/convert_fonts_to_simple_markers', 1))
+
+        self.sde_primary_key: str = s.value('/plugins/slyr/sde_primary_key', 'OBJECTID')
+        self.sde_table_name_conversion: str = s.value('/plugins/slyr/sde_name_conversion', 'unchanged')
+
         self.unsupported_object_callback = None
         self.invalid_layer_resolver = None
         self.warned_crs_definitions = set()
         self.layer_type_hint = None
         self.ignore_online_sources = False  # debugging only!
+        self.main_layer_name = None
+        self.use_real_world_units = False
+        self.global_cim_effects = []
+        self.frame_crs = None
+
+        self.used_geometric_effect_which_flips_orientation = False
 
     def get_picture_store_folder(self):
         """
@@ -92,6 +106,37 @@ class Context:
             return size * 0.352778
         else:
             assert False, 'Unsupported unit type {}'.format(self.units)
+
+    def convert_size_to_points(self, size: float, units) -> Optional[float]:
+        """
+        Converts a size to points
+        """
+        if units == QgsUnitTypes.RenderPoints:
+            return size
+        elif units == QgsUnitTypes.RenderMillimeters:
+            return size / 0.352778
+        elif units == QgsUnitTypes.RenderInches:
+            return size * 72
+        elif units == QgsUnitTypes.RenderPixels:
+            if self.unsupported_object_callback:
+                self.unsupported_object_callback(
+                    '{} ArcGIS Pro does not support pixel units'.format(
+                        self.symbol_name),
+                    level=Context.WARNING)
+            return size / 96 * 72  # assume 96 dpi
+        elif units == QgsUnitTypes.RenderMetersInMapUnits:
+            return size
+        elif units in (QgsUnitTypes.RenderMapUnits,
+                       QgsUnitTypes.RenderPercentage):
+            unit_name = QgsUnitTypes.toString(units)
+            if self.unsupported_object_callback:
+                self.unsupported_object_callback(
+                    '{} ArcGIS Pro does not support {} units'.format(
+                        self.symbol_name, unit_name),
+                    level=Context.WARNING)
+            return None
+
+        raise NotImplementedException('Converting units of type {} is not supported'.format(units))
 
     def convert_path(self, path: str) -> str:
         """
