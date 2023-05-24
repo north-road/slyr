c0d6d9b9e8f8720c0f15e48a6725bfa765cb6da6
Nyall Dawson <nyall.dawson@gmail.com>
2023-05-24 11:31:48 +1000

    Lint

--- slyr_community/parser/objects/representation_rule.py
+++ slyr_community/parser/objects/representation_rule.py
@@ -3,6 +3,7 @@
 Serializable object subclass
 """
 from collections import defaultdict
+
 from ..object import Object
 from ..stream import Stream
 
@@ -28,27 +29,30 @@ class RepresentationRule(Object):
 
         count = stream.read_int('effect count')
         for i in range(count):
-            stream.read_string('effect name {}'.format(i+1))
-            size = stream.read_int('size {}'.format(i+1))
+            stream.read_string('effect name {}'.format(i + 1))
+            size = stream.read_int('size {}'.format(i + 1))
             stream.read_int('unknown', expected=0)
             start = stream.tell()
             self.effects.append(stream.read_object('effect {}'.format(i + 1)))
             assert stream.tell() == start + size
 
         total_objects = stream.read_int('total objects')
-        count = stream.read_int('layer count', expected=total_objects - len(self.effects))
+        count = stream.read_int('layer count',
+                                expected=total_objects - len(self.effects))
         for i in range(count):
             self.layers.append(stream.read_object('layer {}'.format(i + 1)))
 
         field_override_count = stream.read_int('field override count')
         for i in range(field_override_count):
             # note effect_index is 1 based
-            effect_index = stream.read_int('effect_index {}'.format(i+1))
-            attribute_index = stream.read_int('attribute index {}'.format(i+1))
-            field_name = stream.read_string('field name {}'.format(i+1))
+            effect_index = stream.read_int('effect_index {}'.format(i + 1))
+            attribute_index = stream.read_int(
+                'attribute index {}'.format(i + 1))
+            field_name = stream.read_string('field name {}'.format(i + 1))
             self.field_overrides[effect_index][attribute_index] = field_name
 
-        count = stream.read_int('unknown count', expected=len(self.effects) + len(self.layers))
+        count = stream.read_int('unknown count',
+                                expected=len(self.effects) + len(self.layers))
         for i in range(count):
             stream.read_int('unknown {}'.format(i + 1), expected=i + 1)
 
