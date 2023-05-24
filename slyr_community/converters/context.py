38c425221eae865579bcf245e60f5482be3dd2f3
Nyall Dawson <nyall.dawson@gmail.com>
2023-05-24 11:55:00 +1000

    Resync context

--- slyr_community/converters/context.py
+++ slyr_community/converters/context.py
@@ -23,8 +23,13 @@ Conversion context
 """
 
 import os
+import re
 import tempfile
-from typing import Optional
+import base64
+from pathlib import WindowsPath, Path
+from math import cos, sin, asin, sqrt, radians
+
+from typing import Optional, Dict, List, Union
 
 from qgis.PyQt.QtCore import (
     QSettings,
@@ -32,7 +37,10 @@ from qgis.PyQt.QtCore import (
 )
 from qgis.core import (
     Qgis,
-    QgsUnitTypes
+    QgsUnitTypes,
+    QgsSymbol,
+    QgsProject,
+    QgsReferencedRectangle
 )
 
 from ..parser.exceptions import NotImplementedException
@@ -74,8 +82,6 @@ class Context:
         self.units = unit
         self.apply_conversion_tweaks = int(s.value('/plugins/slyr/apply_tweaks', 1))
         self.inkscape_path = s.value('/plugins/slyr/inkscape_path', 'inkscape')
-        self.convert_annotations = int(s.value('/plugins/slyr/enable_annotations', 0))
-        self.convert_layouts = int(s.value('/plugins/slyr/convert_layouts', 1))
         self.convert_esri_fonts_to_simple_markers = int(s.value('/plugins/slyr/convert_fonts_to_simple_markers', 1))
 
         self.sde_primary_key: str = s.value('/plugins/slyr/sde_primary_key', 'OBJECTID')
@@ -91,7 +97,52 @@ class Context:
         self.global_cim_effects = []
         self.frame_crs = None
 
+        self.stable_ids = False
+        self.stable_id_counter = 0
+
+        self.symbol_type_hint: Optional[QgsSymbol.SymbolType] = None
+
         self.used_geometric_effect_which_flips_orientation = False
+
+        self.selection_sets: Dict[str, List] = {}
+
+        self.symbol_layer_output_to_input_index_map = {}
+        self.final_symbol_layer_output_to_input_index_map = {}
+
+        # associated project
+        self.project: Optional[QgsProject] = None
+
+        # folder for destination file
+        self.destination_path: Optional[Path] = None
+
+        self.approx_map_area: Optional[QgsReferencedRectangle] = None
+
+    def push_warning(self, warning: str, level: Optional[str]=None):
+        """
+        Pushes a warning to the context
+        """
+        if not level:
+            level = Context.WARNING
+
+        if not self.unsupported_object_callback:
+            return
+
+        if self.layer_name:
+            self.unsupported_object_callback(
+                '{}: {}'.format(
+                    self.layer_name,
+                    warning),
+                level=level)
+        elif self.symbol_name:
+            self.unsupported_object_callback(
+                '{}: {}'.format(
+                    self.symbol_name,
+                    warning),
+                level=level)
+        else:
+            self.unsupported_object_callback(
+                warning,
+                level=level)
 
     def get_picture_store_folder(self):
         """
@@ -150,6 +201,33 @@ class Context:
 
         raise NotImplementedException('Converting units of type {} is not supported'.format(units))
 
+    def convert_map_units_to_meters(self, size_map_units: float):
+        """
+        Converts map units to approximate meters
+        """
+        if self.approx_map_area:
+            if self.approx_map_area.crs().mapUnits() == QgsUnitTypes.DistanceDegrees\
+                    or not self.approx_map_area.crs().isValid():
+                center = self.approx_map_area.center()
+
+                lon2 = 1
+                lat = center.y()
+                lat, dlon = map(radians, [lat, lon2])
+                # haversine formula
+                a = cos(lat) * cos(lat) * sin(
+                    dlon / 2) ** 2
+                c = 2 * asin(sqrt(a))
+                conversion_factor = 6371000 * c
+            else:
+                conversion_factor = QgsUnitTypes.fromUnitToUnitFactor(
+                    self.approx_map_area.crs().mapUnits(),
+                    QgsUnitTypes.DistanceMeters)
+        else:
+            conversion_factor = QgsUnitTypes.fromUnitToUnitFactor(
+                QgsUnitTypes.DistanceDegrees, QgsUnitTypes.DistanceMeters)
+
+        return size_map_units * conversion_factor
+
     def convert_path(self, path: str) -> str:
         """
         Converts an absolute path for storage in the symbol
@@ -170,3 +248,56 @@ class Context:
             # ESRI renders this closer to a "hairline" width in QGIS land
             return 0
         return size
+
+    @staticmethod
+    def find_relative_path(path: str, diverge_path: Union[str, Path]) -> Union[bool, str]:
+        """
+        Returns a relative path that redirects to the first parameter
+        If the paths are not relative, False is returned
+        If both paths are the same, '.' is returned
+
+        e.g. 'path/example' and 'path/test' would return './example/',
+        as the path redirects to the first path
+        """
+        if isinstance(diverge_path, Path):
+            diverge_path = str(diverge_path)
+
+        if path == diverge_path:
+            return '.'
+
+        output = os.path.relpath(path, diverge_path)
+        if output == '.':
+            return '.'
+        elif path in output:
+            return False
+        return output
+
+    def convert_dataset_path(self, path: Union[str, Path]) -> str:
+        """
+        Converts a dataset's path for writing to ArcGIS formats, using relative paths if possible
+        """
+        if isinstance(path, Path):
+            path = str(path)
+
+        is_rel_path = False
+        if self.destination_path is None:
+            converted_path = path
+        else:
+            new_path = Context.find_relative_path(path, self.destination_path)
+            if new_path is False:
+                converted_path = path
+                is_rel_path = False
+            else:
+                is_rel_path = True
+                converted_path = new_path
+
+        if converted_path == '':
+            converted_path = '.'
+
+        if is_rel_path and not converted_path.startswith(".") and not converted_path.startswith(".."):
+            converted_path = ".\\" + converted_path
+
+        try:
+            return str(WindowsPath(converted_path))
+        except NotImplementedError:
+            return converted_path.replace('/', '\\')
