d9bbabbaa16075b7dd78172bff938d3ec3051d96
Nyall Dawson <nyall.dawson@gmail.com>
2025-09-22 15:13:18 +1000

    Resync browser

--- slyr_community/qgis_plugin/integrations/browser.py
+++ slyr_community/qgis_plugin/integrations/browser.py
@@ -1,14 +1,7 @@
-# -*- coding: utf-8 -*-
+"""
+Browser and app integrations for LYR/STYLE file integration with QGIS
+"""
 
-# /***************************************************************************
-# browser.py
-# ----------
-# Date                 : September 2019
-# copyright            : (C) 2019 by Nyall Dawson
-# email                : nyall.dawson@gmail.com
-#
-#  ***************************************************************************/
-#
 # /***************************************************************************
 #  *                                                                         *
 #  *   This program is free software; you can redistribute it and/or modify  *
@@ -18,15 +11,9 @@
 #  *                                                                         *
 #  ***************************************************************************/
 
-
-"""
-Browser and app integrations for LYR/STYLE file integration with QGIS
-"""
-
 from qgis.PyQt.QtCore import QFileInfo
 from qgis.core import QgsDataItemProvider, QgsDataProvider
 
-from .dat_items import EsriDatItem, DatDropHandler
 from .lyr_items import EsriLyrItem
 from .mxd_items import EsriMxdItem
 from .style_items import EsriStyleItem
@@ -41,17 +28,15 @@ class SlyrDataItemProvider(QgsDataItemProvider):
         return "slyr"
 
     def capabilities(self):  # pylint: disable=missing-docstring
-        return QgsDataProvider.File
+        return QgsDataProvider.DataCapability.File
 
     def createDataItem(self, path, parentItem):  # pylint: disable=missing-docstring
         file_info = QFileInfo(path)
 
         if file_info.suffix().lower() == "style":
             return EsriStyleItem(parentItem, file_info.fileName(), path)
-        elif file_info.suffix().lower() in ("lyr", "lyrx"):
+        elif file_info.suffix().lower() in ("lyr",):
             return EsriLyrItem(parentItem, file_info.fileName(), path)
-        elif DatDropHandler.is_bookmark_dat(path):
-            return EsriDatItem(parentItem, file_info.fileName(), path)
-        elif file_info.suffix().lower() in ("mxd", "pmf", "sxd", "3dd", "mxt"):
+        elif file_info.suffix().lower() in ("mxd",):
             return EsriMxdItem(parentItem, file_info.fileName(), path)
         return None
