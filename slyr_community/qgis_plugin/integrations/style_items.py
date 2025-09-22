2670447aa1c47e0fa73014ffba3d9fe0f6d7341a
Nyall Dawson <nyall.dawson@gmail.com>
2025-09-22 15:57:51 +1000

    Resync style items

--- slyr_community/qgis_plugin/integrations/style_items.py
+++ slyr_community/qgis_plugin/integrations/style_items.py
@@ -1,14 +1,7 @@
-# -*- coding: utf-8 -*-
+"""
+Browser and app integrations for STYLE file integration with QGIS
+"""
 
-# /***************************************************************************
-# browser.py
-# ----------
-# Date                 : September 2019
-# copyright            : (C) 2019 by Nyall Dawson
-# email                : nyall.dawson@gmail.com
-#
-#  ***************************************************************************/
-#
 # /***************************************************************************
 #  *                                                                         *
 #  *   This program is free software; you can redistribute it and/or modify  *
@@ -17,11 +10,6 @@
 #  *   (at your option) any later version.                                   *
 #  *                                                                         *
 #  ***************************************************************************/
-
-
-"""
-Browser and app integrations for STYLE file integration with QGIS
-"""
 
 import html
 from functools import partial
@@ -33,13 +21,17 @@ from qgis.PyQt.QtWidgets import QAction, QProgressDialog, QPushButton
 from qgis.core import (
     Qgis,
     QgsDataItem,
+    QgsFeatureRequest,
     QgsMimeDataUtils,
     QgsStyle,
     QgsFeedback,
     QgsSymbol,
     QgsColorRamp,
     QgsTextFormat,
+    QgsVectorLayer,
     QgsPalLayerSettings,
+    QgsProject,
+    QgsWkbTypes,
 )
 from qgis.gui import QgsCustomDropHandler, QgsStyleManagerDialog
 from qgis.utils import iface
@@ -47,7 +39,10 @@ from qgis.utils import iface
 from .browser_utils import BrowserUtils
 from ..gui_utils import GuiUtils
 from ...bintools.extractor import Extractor, MissingBinaryException
+from ...converters.color_ramp import ColorRampConverter
 from ...converters.context import Context
+from ...converters.geometry import GeometryConverter
+from ...converters.labels import LabelConverter
 from ...converters.symbols import SymbolConverter
 from ...parser.exceptions import (
     UnreadableSymbolException,
@@ -55,7 +50,9 @@ from ...parser.exceptions import (
     NotImplementedException,
     UnknownClsidException,
     UnreadablePictureException,
+    UnknownObjectTypeException,
 )
+from ...parser.exceptions import RequiresLicenseException
 from ...parser.stream import Stream
 
 try:
@@ -82,6 +79,7 @@ class StyleDropHandler(QgsCustomDropHandler):
         """
         Opens a .style file
         """
+
         if (
             input_file.lower().endswith(".style")
             and not Extractor.is_mdb_tools_binary_available()
@@ -92,7 +90,7 @@ class StyleDropHandler(QgsCustomDropHandler):
                 "Configure…", pressed=partial(BrowserUtils.open_settings, widget)
             )
             widget.layout().addWidget(settings_button)
-            message_bar.pushWidget(widget, Qgis.Critical)
+            message_bar.pushWidget(widget, Qgis.MessageLevel.Critical)
             return True
 
         style = QgsStyle()
@@ -145,6 +143,7 @@ class StyleDropHandler(QgsCustomDropHandler):
         unreadable = []
         warnings = set()
         errors = set()
+        info = set()
 
         types_to_extract = [
             Extractor.FILL_SYMBOLS,
@@ -156,6 +155,7 @@ class StyleDropHandler(QgsCustomDropHandler):
             Extractor.MAPLEX_LABELS,
             Extractor.AREA_PATCHES,
             Extractor.LINE_PATCHES,
+            Extractor.REPRESENTATION_RULES,
         ]
 
         type_percent = 100 / len(types_to_extract)
@@ -168,7 +168,7 @@ class StyleDropHandler(QgsCustomDropHandler):
                     "MDB Tools utility not found",
                     "Convert style",
                     'The MDB tools "mdb-export" utility is required to convert .style databases. Please setup a path to the MDB tools utility in the SLYR options panel.',
-                    level=Qgis.Critical,
+                    level=Qgis.MessageLevel.Critical,
                 )
                 progress_dialog.deleteLater()
                 return True
@@ -186,6 +186,9 @@ class StyleDropHandler(QgsCustomDropHandler):
                 if feedback.isCanceled():
                     break
                 name = raw_symbol[Extractor.NAME]
+                if not name:
+                    name = "unnamed"
+
                 tags = raw_symbol[Extractor.TAGS].split(";")
 
                 unique_name = make_name_unique(name)
@@ -225,6 +228,7 @@ class StyleDropHandler(QgsCustomDropHandler):
                     continue
 
                 context = Context()
+                context.project = QgsProject.instance()
                 context.symbol_name = unique_name
 
                 def unsupported_object_callback(msg, level=Context.WARNING):
@@ -242,30 +246,46 @@ class StyleDropHandler(QgsCustomDropHandler):
                                 html.escape(msg),
                             )
                         )
+                    elif level == Context.INFO:
+                        info.add(
+                            "<b>{}</b>: {}".format(
+                                html.escape(unique_name),  # pylint: disable=cell-var-from-loop
+                                html.escape(msg),
+                            )
+                        )
 
                 context.unsupported_object_callback = unsupported_object_callback
-                # context.style_folder, _ = os.path.split(output_file)
 
                 if symbol_type in (Extractor.AREA_PATCHES, Extractor.LINE_PATCHES):
-                    unreadable.append(
-                        "<b>{}</b>: Legend patch conversion requires the licensed version of SLYR".format(
-                            html.escape(name)
+                    if symbol_type == Extractor.LINE_PATCHES:
+                        geom = GeometryConverter.convert_geometry(symbol.polyline)
+                        qgis_symbol = QgsLegendPatchShape(
+                            QgsSymbol.SymbolType.Line, geom, symbol.preserve_aspect
                         )
-                    )
-                    continue
-
-                try:
-                    qgis_symbol = SymbolConverter.Symbol_to_QgsSymbol(symbol, context)
-                except NotImplementedException as e:
-                    unreadable.append(
-                        "<b>{}</b>: {}".format(html.escape(name), html.escape(str(e)))
-                    )
-                    continue
-                except UnreadablePictureException as e:
-                    unreadable.append(
-                        "<b>{}</b>: {}".format(html.escape(name), html.escape(str(e)))
-                    )
-                    continue
+                    elif symbol_type == Extractor.AREA_PATCHES:
+                        geom = GeometryConverter.convert_geometry(symbol.polygon)
+                        qgis_symbol = QgsLegendPatchShape(
+                            QgsSymbol.SymbolType.Fill, geom, symbol.preserve_aspect
+                        )
+                else:
+                    try:
+                        qgis_symbol = SymbolConverter.Symbol_to_QgsSymbol(
+                            symbol, context
+                        )
+                    except NotImplementedException as e:
+                        unreadable.append(
+                            "<b>{}</b>: {}".format(
+                                html.escape(name), html.escape(str(e))
+                            )
+                        )
+                        continue
+                    except UnreadablePictureException as e:
+                        unreadable.append(
+                            "<b>{}</b>: {}".format(
+                                html.escape(name), html.escape(str(e))
+                            )
+                        )
+                        continue
 
                 if isinstance(qgis_symbol, QgsSymbol):
                     # self.check_for_missing_fonts(qgis_symbol, feedback)
@@ -273,51 +293,56 @@ class StyleDropHandler(QgsCustomDropHandler):
                 elif isinstance(qgis_symbol, QgsColorRamp):
                     style.addColorRamp(unique_name, qgis_symbol, True)
                 elif isinstance(qgis_symbol, QgsTextFormat):
-                    if Qgis.QGIS_VERSION_INT >= 30900:
-                        style.addTextFormat(unique_name, qgis_symbol, True)
+                    style.addTextFormat(unique_name, qgis_symbol, True)
                 elif isinstance(qgis_symbol, QgsPalLayerSettings):
-                    if Qgis.QGIS_VERSION_INT >= 30900:
-                        style.addLabelSettings(unique_name, qgis_symbol, True)
-                elif Qgis.QGIS_VERSION_INT >= 31300:
-                    if isinstance(qgis_symbol, QgsLegendPatchShape):
-                        style.addLegendPatchShape(unique_name, qgis_symbol, True)
+                    style.addLabelSettings(unique_name, qgis_symbol, True)
+                elif isinstance(qgis_symbol, QgsLegendPatchShape):
+                    style.addLegendPatchShape(unique_name, qgis_symbol, True)
 
                 if tags:
                     if isinstance(qgis_symbol, QgsSymbol):
-                        assert style.tagSymbol(QgsStyle.SymbolEntity, unique_name, tags)
+                        assert style.tagSymbol(
+                            QgsStyle.StyleEntity.SymbolEntity, unique_name, tags
+                        )
                     elif isinstance(qgis_symbol, QgsColorRamp):
                         assert style.tagSymbol(
-                            QgsStyle.ColorrampEntity, unique_name, tags
+                            QgsStyle.StyleEntity.ColorrampEntity, unique_name, tags
                         )
                     elif isinstance(qgis_symbol, QgsTextFormat) and hasattr(
                         QgsStyle, "TextFormatEntity"
                     ):
                         assert style.tagSymbol(
-                            QgsStyle.TextFormatEntity, unique_name, tags
+                            QgsStyle.StyleEntity.TextFormatEntity, unique_name, tags
                         )
                     elif isinstance(qgis_symbol, QgsPalLayerSettings) and hasattr(
                         QgsStyle, "LabelSettingsEntity"
                     ):
                         assert style.tagSymbol(
-                            QgsStyle.LabelSettingsEntity, unique_name, tags
+                            QgsStyle.StyleEntity.LabelSettingsEntity, unique_name, tags
                         )
-                    elif Qgis.QGIS_VERSION_INT >= 31300:
-                        if isinstance(qgis_symbol, QgsLegendPatchShape):
-                            assert style.tagSymbol(
-                                QgsStyle.LegendPatchShapeEntity, unique_name, tags
-                            )
+                    elif isinstance(qgis_symbol, QgsLegendPatchShape):
+                        assert style.tagSymbol(
+                            QgsStyle.StyleEntity.LegendPatchShapeEntity,
+                            unique_name,
+                            tags,
+                        )
         progress_dialog.deleteLater()
         if feedback.isCanceled():
             return True
 
-        if errors or unreadable or warnings:
+        if errors or unreadable or warnings or info:
             message = ""
+            title = ""
+            level = None
+
             if unreadable:
                 message = "<p>The following symbols could not be converted:</p>"
                 message += "<ul>"
                 for w in unreadable:
                     message += "<li>{}</li>".format(w.replace("\n", "<br>"))
                 message += "</ul>"
+                title = "style could not be completely converted"
+                level = Qgis.MessageLevel.Critical
 
             if errors:
                 message += "<p>The following errors were generated while converting symbols:</p>"
@@ -325,6 +350,10 @@ class StyleDropHandler(QgsCustomDropHandler):
                 for w in errors:
                     message += "<li>{}</li>".format(w.replace("\n", "<br>"))
                 message += "</ul>"
+                if not title:
+                    title = "style could not be completely converted"
+                if level is None:
+                    level = Qgis.MessageLevel.Critical
 
             if warnings:
                 message += "<p>The following warnings were generated while converting symbols:</p>"
@@ -332,28 +361,43 @@ class StyleDropHandler(QgsCustomDropHandler):
                 for w in warnings:
                     message += "<li>{}</li>".format(w.replace("\n", "<br>"))
                 message += "</ul>"
+                if not title:
+                    title = "style could not be completely converted"
+                if level is None:
+                    level = Qgis.MessageLevel.Warning
 
-            BrowserUtils.show_warning(
-                "style could not be completely converted",
-                "Convert style",
-                message,
-                level=Qgis.Critical if (unreadable or errors) else Qgis.Warning,
-            )
+            if info:
+                if message:
+                    message += (
+                        "<p>Additionally, some extra messages were generated:</p>"
+                    )
+                else:
+                    message += "<p>The following information messages were generated converting symbols:</p>"
+                message += "<ul>"
+                for w in info:
+                    message += "<li>{}</li>".format(
+                        html.escape(w).replace("\n", "<br>")
+                    )
+                message += "</ul>"
+                if not title:
+                    title = "Some messages were generated while converting style"
+                if level is None:
+                    level = Qgis.MessageLevel.Info
 
-        if Qgis.QGIS_VERSION_INT >= 30800:
-            dlg = QgsStyleManagerDialog(style, readOnly=True)
-            dlg.setFavoritesGroupVisible(False)
-            dlg.setSmartGroupsVisible(False)
-            fi = QFileInfo(input_file)
-            dlg.setBaseStyleName(fi.baseName())
-        else:
-            dlg = QgsStyleManagerDialog(style)
+            BrowserUtils.show_warning(title, "Convert style", message, level=level)
+
+        dlg = QgsStyleManagerDialog(style, readOnly=True)
+        dlg.setFavoritesGroupVisible(False)
+        dlg.setSmartGroupsVisible(False)
+        fi = QFileInfo(input_file)
+        dlg.setBaseStyleName(fi.baseName())
+
         dlg.setWindowTitle(fi.baseName())
-        dlg.exec_()
+        dlg.exec()
         return True
 
     @staticmethod
-    def open_stylx(input_file):  # pylint: disable=too-many-locals,too-many-branches,too-many-statements,unused-argument
+    def open_stylx(input_file):  # pylint: disable=too-many-locals,too-many-branches,too-many-statements
         """
         Opens a .stylx file
         """
@@ -377,8 +421,8 @@ class EsriStyleItem(QgsDataItem):
     """
 
     def __init__(self, parent, name, path):
-        super().__init__(QgsDataItem.Custom, parent, name, path)
-        self.setState(QgsDataItem.Populated)  # no children
+        super().__init__(QgsDataItem.Type.Custom, parent, name, path)
+        self.setState(QgsDataItem.State.Populated)  # no children
         self.setToolTip(QDir.toNativeSeparators(path))
 
     def hasDragEnabled(self):  # pylint: disable=missing-docstring
@@ -412,6 +456,7 @@ class EsriStyleItem(QgsDataItem):
         """
         Executes the style to xml conversion algorithm
         """
+
         execAlgorithmDialog("slyr:styletoqgisxml", {"INPUT": self.path()})
 
     def actions(self, parent):  # pylint: disable=missing-docstring
