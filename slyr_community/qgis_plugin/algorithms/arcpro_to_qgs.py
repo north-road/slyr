f76b35f090c40bf12053d7ca67466aed87e3e420
Nyall Dawson <nyall.dawson@gmail.com>
2025-09-22 14:18:46 +1000

    Algorithm sync

---
+++ slyr_community/qgis_plugin/algorithms/arcpro_to_qgs.py
@@ -0,0 +1,82 @@
+"""
+Converts an APRX document to a QGS project file
+"""
+
+# /***************************************************************************
+#  *                                                                         *
+#  *   This program is free software; you can redistribute it and/or modify  *
+#  *   it under the terms of the GNU General Public License as published by  *
+#  *   the Free Software Foundation; either version 2 of the License, or     *
+#  *   (at your option) any later version.                                   *
+#  *                                                                         *
+#  ***************************************************************************/
+
+from qgis.core import (
+    Qgis,
+    QgsProcessingParameterFileDestination,
+    QgsProcessingParameterBoolean,
+    QgsProcessingParameterDefinition,
+    QgsProcessingException,
+)
+from qgis.core import QgsProject
+
+from .algorithm import SlyrAlgorithm
+from ...converters.context import Context
+from ...converters.project import ProjectConverter
+from ...parser.exceptions import (
+    UnreadableSymbolException,
+    NotImplementedException,
+    UnknownClsidException,
+    EmptyDocumentException,
+    DocumentTypeException,
+    CorruptDocumentException,
+)
+
+
+class ConvertArcProToQgs(SlyrAlgorithm):
+    """
+    Converts an ArcGIS Pro document to a QGS project file
+    """
+
+    INPUT = "INPUT"
+    OUTPUT = "OUTPUT"
+    TEST_MODE = "TEST_MODE"
+
+    # pylint: disable=missing-docstring,unused-argument
+
+    def group(self):
+        return "ArcGIS Pro"
+
+    def groupId(self):
+        return "arcgispro"
+
+    def initAlgorithm(self, config=None):
+        param_test_mode = QgsProcessingParameterBoolean(
+            self.TEST_MODE, "Test mode (debug option)", False, True
+        )
+        param_test_mode.setFlags(
+            param_test_mode.flags() | QgsProcessingParameterDefinition.Flag.FlagHidden
+        )
+        self.addParameter(param_test_mode)
+
+        self.addParameter(
+            QgsProcessingParameterFileDestination(
+                self.OUTPUT,
+                "Destination QGS project file",
+                fileFilter="QGS files (*.qgs);;QGZ files (*.qgz)",
+            )
+        )
+
+    # pylint: enable=missing-docstring,unused-argument
+
+    @staticmethod
+    def convert_aprx(input_file, feedback, test_mode: bool, context):
+        """
+        Converts an APRX project
+        """
+
+    @staticmethod
+    def convert_mapx(input_file, feedback, test_mode: bool, context):
+        """
+        Converts an MAPX project
+        """
