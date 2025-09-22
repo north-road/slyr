2e6b6a9659f5d3b66dadeef15373b5b44ae2d391
Nyall Dawson <nyall.dawson@gmail.com>
2025-09-22 15:00:04 +1000

    Algorithm sync

--- slyr_community/qgis_plugin/algorithms/xml_to_stylx.py
+++ slyr_community/qgis_plugin/algorithms/xml_to_stylx.py
@@ -1,14 +1,7 @@
-# -*- coding: utf-8 -*-
+"""
+Converts QGIS xml style databases to a stylx database
+"""
 
-# /***************************************************************************
-# context.py
-# ----------
-# Date                 : September 2019
-# copyright            : (C) 2019 by Nyall Dawson, North Road Consulting
-# email                : nyall.dawson@gmail.com
-#
-#  ***************************************************************************/
-#
 # /***************************************************************************
 #  *                                                                         *
 #  *   This program is free software; you can redistribute it and/or modify  *
@@ -18,19 +11,20 @@
 #  *                                                                         *
 #  ***************************************************************************/
 
-
-"""
-Converts QGIS xml style databases to a stylx database
-"""
+import os
+from pathlib import Path
 
 from qgis.core import (
     QgsProcessingParameterFile,
     QgsProcessingParameterFileDestination,
     QgsProcessingOutputNumber,
     QgsProcessingException,
+    QgsStyle,
 )
 
 from .algorithm import SlyrAlgorithm
+from .utils import AlgorithmUtils
+from qgis.core import QgsProcessingException
 
 
 class XmlToStylx(SlyrAlgorithm):
@@ -54,10 +48,10 @@ class XmlToStylx(SlyrAlgorithm):
         return "xmltostylx"
 
     def displayName(self):
-        return "Convert QGIS style XML to stylx"
+        return "Convert QGIS style XML to STYLX"
 
     def shortDescription(self):
-        return "Convert a QGIS XML style database to an ArcGIS Pro stylx database"
+        return "Convert a QGIS XML style database to an ArcGIS Pro STYLX database"
 
     def group(self):
         return "ArcGIS Pro"
@@ -66,7 +60,7 @@ class XmlToStylx(SlyrAlgorithm):
         return "arcgispro"
 
     def shortHelpString(self):
-        return "Converts a QGIS XML style database to an ArcGIS Pro stylx database."
+        return "Converts a QGIS XML style database to an ArcGIS Pro STYLX database."
 
     def initAlgorithm(self, config=None):
         self.addParameter(
@@ -88,6 +82,16 @@ class XmlToStylx(SlyrAlgorithm):
         self.addOutput(
             QgsProcessingOutputNumber(self.TEXT_SYMBOL_COUNT, "Text Symbol Count")
         )
+
+    def autogenerateParameterValues(self, rowParameters, changedParameter, mode):
+        if changedParameter == self.INPUT:
+            input_file = rowParameters.get(self.INPUT)
+            if input_file:
+                input_path = Path(input_file)
+                if input_path.exists():
+                    return {self.OUTPUT: input_path.with_suffix(".stylx").as_posix()}
+
+        return {}
 
     def processAlgorithm(
         self,  # pylint: disable=too-many-locals,too-many-statements,too-many-branches
